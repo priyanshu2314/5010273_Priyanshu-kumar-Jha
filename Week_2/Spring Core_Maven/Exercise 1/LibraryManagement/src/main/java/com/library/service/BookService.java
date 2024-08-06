7f2d07c1af6dfd7161ff479ce33d9141c129b22c
Priyanshu <priyanshujha1109@gmail.com>
2024-08-07 01:13:01 +0530

    Week_2 assignment

---
+++ Week_2/Spring Core_Maven/Exercise 1/LibraryManagement/src/main/java/com/library/service/BookService.java
@@ -0,0 +1,11 @@
+package com.library.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BookService {
+	public void bookServiceShow()
+	{
+		System.out.println("Inside BookService");
+	}
+}
